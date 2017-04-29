794b70ff8f6960e4c8fa4eca4dcfad18bcf702c3
Travis Nice <travis.nice@everydaypublishing.com.au>
2017-04-29 18:05:00 +1000

    update nomination emails and ajax script

--- includes/ep-award-nominations-video-upload-content.php
+++ includes/ep-award-nominations-video-upload-content.php
@@ -8,12 +8,14 @@
 
 <?php wp_nonce_field('image_upload', 'image_upload_nonce');?>
 
-<p><input type="submit" value="upload"></p>
+<p><input type="submit" value="Upload" style="margin: 16px 16px 16px 0;"></p>
 
 </form>
 
 <p id="status"></p>
 
-<p><progress id="progressBar" value="0" max="100" style="width:300px;"></progress></p>
-
 <p><ul style="list-style-type:none;" id="images_wrap"><!-- Videos will be added here --></ul></p>
+
+<form name="continue_form" method="POST" id="continue_form">
+<p><button name="video-submit" value="next" style="margin: 16px 16px 16px 0;">Save & Next</button><p>
+</form>
